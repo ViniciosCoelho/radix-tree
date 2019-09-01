d3785e32c6448107af005caaec9018a177c3fac6
ViniciosCoelho <vinicios.coelho.job@gmail.com>
2019-09-01 01:43:14 -0300

    Added new changes

--- RadixTree.py
+++ RadixTree.py
@@ -8,6 +8,7 @@ class RadixTree:
 
     def __find_word_nearest_node(self, word: str):
         node = self.root
+        found_word = ''
 
         while not node.is_leaf():
             current_word = word
@@ -17,23 +18,32 @@ class RadixTree:
 
                 if word.startswith(current_word):
                     word.replace(current_word, '')
+                    found_word += current_word
                     node = edge
             
             if current_word == word:
-                return False, node
-
-        if word == node.word:
-            return True, node
-        else:
-            return False, node
+                break
+        
+        return found_word, node
     
     def find(self, word: str):
-        found_word, node = self.__find_word_nearest_node(word)
-        return found_word
-    
-    def insert(self, word):
-        found_word, node = self.__find_word_nearest_node(word)
+        word_found, node = self.__find_word_nearest_node(word)
 
-        if found_word:
+        if word == word_found:
+            return True
+        else:
             return False
-        if
+
+        return is_word_found
+    
+    def insert(self, word: str):
+        word_found, node = self.__find_word_nearest_node(word)
+
+        if word_found == word:
+            return False
+
+        if node.is_leaf():
+            split_index = node.word.find(word)
+            preffix = word.
+        
+        return True
